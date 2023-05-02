17d7026d2f9f6767fa0765d47566397ebc004bc7
Victoria Hektor <94381284+goddessmode@users.noreply.github.com>
2023-05-02 22:30:32 +0100

    Delete connect_db.php

--- connect_db.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$db_username = 'root';
-	$db_password = '';
-	$db_connect = new PDO( 'mysql:host=localhost;dbname=db_login', $db_username, $db_password );
-	if(!$db_connect){
-		die("Fatal Error: Connection Failed!");
-	}
-?>
